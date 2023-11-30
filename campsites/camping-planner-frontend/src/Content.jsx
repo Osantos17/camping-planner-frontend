298d0a9c3a0b1005b8847397bf9f003c978937f6
Orlando Santos <Santos.Orlando17@gmail.com>
2023-11-30 13:32:00 -0800

    test deploy

--- campsites/camping-planner-frontend/src/Content.jsx
+++ campsites/camping-planner-frontend/src/Content.jsx
@@ -5,7 +5,7 @@ export function Content() {
   return (
     <div >
       <Routes>
-        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   )
