6baad9cf484c3aaf312d01aff8fa395138003550
Orlando Santos <Santos.Orlando17@gmail.com>
2023-11-30 13:35:03 -0800

    test deploy 2

--- campsites/camping-planner-frontend/src/App.jsx
+++ campsites/camping-planner-frontend/src/App.jsx
@@ -1,24 +1,24 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-// import { Signup } from "./Signup";
+import { Signup } from "./Signup";
 import "./Header.css"
-// import { NationalParks } from "./NationalParks"
-// import { MyParks } from './MyParks';
+import { NationalParks } from "./NationalParks"
+import { MyParks } from './MyParks';
 
 function App() {
   return (
     <div className="body">
       <BrowserRouter>
         <Header />
-        <Content />
-        {/* <Routes>
+        <Routes>
+          <Route path="*" element={<Content />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/nationalparks" element={<NationalParks />} />
           <Route path="/myparks" element={<MyParks />} />
-        </Routes> */}
+        </Routes>
       </BrowserRouter>
     </div>
   );
